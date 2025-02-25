81cca71e036ba31ff4cf7760e1a2bf321ae199cd
issac <jins3569@gmail.com>
2025-02-25 22:03:25 +0900

    fix: disable format middleware

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import { envMiddleware } from './global/middleware/env';
 envMiddleware();
 
-import { notiCron, testCron, tradingStrategy1 } from '@/cron';
+import { notiCron } from '@/cron';
 import { PORT } from '@/global/config/info';
 import bithumbRouter from '@/routes/bithumb';
 import express, { Request, Response } from 'express';
 import figlet from 'figlet';
-import { responseFormatMiddleware } from './global/middleware/format';
 import { entryPointLoggingMiddleware, uuidMiddleware } from './global/middleware/logging';
 
 const app = express();
@@ -20,7 +19,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(uuidMiddleware); // UUID 설정 미들웨어
 app.use(entryPointLoggingMiddleware); // route start log 미들웨어
-app.use(responseFormatMiddleware); // success, fail 포맷 설정
+// app.use(responseFormatMiddleware); // success, fail 포맷 설정
+
+// testCron();
+notiCron();
+// tradingStrategy1();
 
 app.get('/', (req: Request, res: Response) => {
     console.log(req.body);
@@ -28,10 +31,6 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.use('/bithumb', bithumbRouter); // API 관련 라우트
-
-testCron();
-notiCron();
-tradingStrategy1();
 
 app.listen(port, () => {
     console.log(figlet.textSync('PTS'));
