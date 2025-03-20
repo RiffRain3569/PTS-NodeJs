237d80569c20609d4f0bd56dd0c8f66e1c050fbc
issac <jins3569@gmail.com>
2025-03-20 23:20:47 +0900

    feat: change product cron

---
+++ src/global/middleware/cron/holdHour.ts
@@ -0,0 +1,60 @@
+import { HOST, PORT } from '@/global/config/info';
+import { send } from '@/global/config/telegram';
+import axios from 'axios';
+import cron from 'node-cron';
+
+type Types = {
+    hour: number;
+    second?: number;
+    duringHour?: number;
+    top: number;
+    askPercent: number;
+};
+export const hold_hour = ({ hour, second = 0, duringHour = 2, top, askPercent }: Types) => {
+    let market: any;
+    let uuids: string[] = [];
+
+    // 한 종목 매수
+    cron.schedule(`${second} 1 ${hour} * * *`, async () => {
+        try {
+            market = (await axios.post(`${HOST}:${PORT}/bithumb/order/bid/top/${top}`)).data;
+            console.log(`${market.korean_name} 매수 완료 했습니다.`);
+            await send(`${market.korean_name} 매수 완료 했습니다.`);
+        } catch (error) {
+            await send('에러가 발생하였습니다.');
+            console.log(error);
+        }
+    });
+
+    // 목표 매도 걸기
+    cron.schedule(`${second + 2} 1 ${hour} * * *`, async () => {
+        try {
+            // const testMarkets = [ { market: 'KRW-KAITO', trade_price: 2772 } ];
+            uuids = (
+                await axios.post(`${HOST}:${PORT}/bithumb/order/ask/limit`, { markets: [market], percent: askPercent })
+            ).data;
+            console.log(`${uuids.join(', ')} 매도 예약 완료 했습니다.`);
+            await send(`${uuids.join(', ')} 매도 예약 완료 했습니다.`);
+        } catch (error) {
+            await send('에러가 발생하였습니다.');
+            console.log(error);
+        }
+    });
+
+    // 지정 시간 후 예약된 매도 취소 후 전량 매도
+    cron.schedule(`${second} 1 ${(hour + duringHour) % 24} * * *`, async () => {
+        try {
+            const waitingMarket = (await axios.delete(`${HOST}:${PORT}/bithumb/order`)).data;
+            console.log(waitingMarket.map(({ market }: any) => market));
+
+            await axios.post(`${HOST}:${PORT}/bithumb/order/ask`, {
+                markets: waitingMarket.map(({ market }: any) => market),
+            });
+            console.log(`${waitingMarket.map(({ market }: any) => market).join(', ')} 매도 완료 했습니다.`);
+            await send(`${waitingMarket.map(({ market }: any) => market).join(', ')} 매도 완료 했습니다.`);
+        } catch (error) {
+            await send('에러가 발생하였습니다.');
+            console.log(error);
+        }
+    });
+};
--- src/global/middleware/cron/index.ts
+++ src/global/middleware/cron/index.ts
@@ -2,7 +2,7 @@ import { HOST, PORT } from '@/global/config/info';
 import { send } from '@/global/config/telegram';
 import axios from 'axios';
 import cron from 'node-cron';
-import { two_hour } from './strategy2';
+import { hold_hour } from './holdHour';
 
 /**
  * 1시 - 21시 사이 1시간 마다 변동률 상위 5개 조회
@@ -17,8 +17,21 @@ export const notiCron = () => {
                 const markets = await axios.get(`${HOST}:${PORT}/bithumb/market/top5`);
                 await send(JSON.stringify(markets.data, null, 4));
             } catch (error) {
-                await send(JSON.stringify(error, null, 4));
+                console.log(error);
             }
+        });
+    }
+};
+
+export const logCron = () => {
+    const ignoreHours = [0, 7, 8, 9, 22, 23];
+    const hours = Array.from({ length: 24 }, (_, i) => i).filter((hour) => !ignoreHours.includes(hour));
+    for (const hour of hours) {
+        // 변동률 상위 5개 조회
+        cron.schedule(`1 ${hour} * * *`, async () => {
+            try {
+                const markets = await axios.get(`${HOST}:${PORT}/bithumb/market/top5`);
+            } catch (error) {}
         });
     }
 };
@@ -26,12 +39,31 @@ export const notiCron = () => {
 export const cronMiddleware = () => {
     if (process.env.NODE_ENV === 'production') {
         notiCron();
+        hold_hour({ hour: 1, second: 1, top: 3, askPercent: 0.05 });
+        hold_hour({ hour: 3, second: 2, top: 1, askPercent: 0.02 });
+        hold_hour({ hour: 5, second: 3, top: 1, askPercent: 0.05 });
+        hold_hour({ hour: 10, second: 1, top: 4, askPercent: 0.01 });
+        hold_hour({ hour: 12, second: 2, top: 5, askPercent: 0.01 });
+
+        hold_hour({ hour: 16, second: 1, duringHour: 1, top: 5, askPercent: 0.02 });
+        hold_hour({ hour: 17, second: 2, duringHour: 1, top: 5, askPercent: 0.02 });
+        hold_hour({ hour: 18, second: 3, duringHour: 1, top: 5, askPercent: 0.05 });
+        hold_hour({ hour: 19, second: 4, duringHour: 1, top: 5, askPercent: 0.03 });
+        hold_hour({ hour: 20, second: 5, duringHour: 1, top: 5, askPercent: 0.05 });
+        hold_hour({ hour: 21, second: 6, top: 5, askPercent: 0.02 });
     } else {
-        two_hour({ hour: 1, top: 2, askPercent: 0.01 });
-        two_hour({ hour: 3, second: 2, top: 1, askPercent: 0.02 });
-        two_hour({ hour: 5, second: 4, top: 1, askPercent: 0.01 });
-        two_hour({ hour: 10, top: 4, askPercent: 0.01 });
-        two_hour({ hour: 12, second: 2, top: 5, askPercent: 0.01 });
-        two_hour({ hour: 21, top: 5, askPercent: 0.02 });
+        logCron();
+        hold_hour({ hour: 1, second: 1, top: 3, askPercent: 0.05 });
+        hold_hour({ hour: 3, second: 2, top: 1, askPercent: 0.02 });
+        hold_hour({ hour: 5, second: 3, top: 1, askPercent: 0.05 });
+        hold_hour({ hour: 10, second: 1, top: 4, askPercent: 0.01 });
+        hold_hour({ hour: 12, second: 2, top: 5, askPercent: 0.01 });
+
+        hold_hour({ hour: 16, second: 1, duringHour: 1, top: 5, askPercent: 0.02 });
+        // hold_hour({ hour: 17, second: 2, duringHour: 1, top: 5, askPercent: 0.02 });
+        hold_hour({ hour: 18, second: 2, duringHour: 1, top: 5, askPercent: 0.05 });
+        // hold_hour({ hour: 19, second: 4, duringHour: 1, top: 5, askPercent: 0.03 });
+        hold_hour({ hour: 20, second: 3, duringHour: 1, top: 5, askPercent: 0.05 });
+        // hold_hour({ hour: 21, second: 6, top: 5, askPercent: 0.02 });
     }
 };
