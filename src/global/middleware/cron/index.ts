497c5278f6300492c5a2633985a353f8357d7dc8
issac <jins3569@gmail.com>
2025-03-21 21:05:58 +0900

    fix: crash cron bug

--- src/global/middleware/cron/index.ts
+++ src/global/middleware/cron/index.ts
@@ -29,9 +29,7 @@ export const logCron = () => {
     for (const hour of hours) {
         // 변동률 상위 5개 조회
         cron.schedule(`1 ${hour} * * *`, async () => {
-            try {
-                const markets = await axios.get(`${HOST}:${PORT}/bithumb/market/top5`);
-            } catch (error) {}
+            await axios.get(`${HOST}:${PORT}/bithumb/market/top5`);
         });
     }
 };
@@ -40,17 +38,17 @@ export const cronMiddleware = () => {
     if (process.env.NODE_ENV === 'production') {
         notiCron();
         hold_hour({ hour: 1, second: 1, top: 3, askPercent: 0.05 });
-        hold_hour({ hour: 3, second: 2, top: 1, askPercent: 0.02 });
-        hold_hour({ hour: 5, second: 3, top: 1, askPercent: 0.05 });
+        hold_hour({ hour: 3, second: 3, top: 1, askPercent: 0.02 });
+        hold_hour({ hour: 5, second: 5, top: 1, askPercent: 0.05 });
         hold_hour({ hour: 10, second: 1, top: 4, askPercent: 0.01 });
-        hold_hour({ hour: 12, second: 2, top: 5, askPercent: 0.01 });
+        hold_hour({ hour: 12, second: 3, top: 5, askPercent: 0.01 });
 
         hold_hour({ hour: 16, second: 1, duringHour: 1, top: 5, askPercent: 0.02 });
-        hold_hour({ hour: 17, second: 2, duringHour: 1, top: 5, askPercent: 0.02 });
-        hold_hour({ hour: 18, second: 3, duringHour: 1, top: 5, askPercent: 0.05 });
-        hold_hour({ hour: 19, second: 4, duringHour: 1, top: 3, askPercent: 0.03 });
-        hold_hour({ hour: 20, second: 5, duringHour: 1, top: 3, askPercent: 0.05 });
-        hold_hour({ hour: 21, second: 6, top: 5, askPercent: 0.02 });
+        hold_hour({ hour: 17, second: 3, duringHour: 1, top: 5, askPercent: 0.02 });
+        hold_hour({ hour: 18, second: 5, duringHour: 1, top: 5, askPercent: 0.05 });
+        hold_hour({ hour: 19, second: 7, duringHour: 1, top: 3, askPercent: 0.03 });
+        hold_hour({ hour: 20, second: 9, duringHour: 1, top: 3, askPercent: 0.05 });
+        hold_hour({ hour: 21, second: 11, top: 5, askPercent: 0.02 });
     } else {
         logCron();
         // hold_hour({ hour: 1, second: 1, top: 3, askPercent: 0.05 });
