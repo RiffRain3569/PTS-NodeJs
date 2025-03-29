32cb8804bb056adcf5b5a1b56bccf52fbf4e245e
issac <jins3569@gmail.com>
2025-03-29 17:18:53 +0900

    chore: auto deploy with github action

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,6 +8,7 @@ module.exports = {
                 NODE_ENV: 'production',
             },
             watch: false,
+            ignore_watch: ['node_modules', 'logs'], // 변경 감지 무시할 폴더
             restart_delay: 5000, // 서버가 다운된 후 5초 뒤 재시작
             out_file: './logs/stdout.log',
             error_file: './logs/error.log',
